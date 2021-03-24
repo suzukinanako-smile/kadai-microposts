6031b29e10b5faad0789688a19da8a7e899b2fc0
taro.kirameki <suzuki.nanako@smileforce.jp>
2021-03-24 06:18:36 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
